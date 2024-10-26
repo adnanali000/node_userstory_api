3b081f9d002e8c04835752003ebbdca7c6d7e00a
adnanmusil <adnan.museel123@gmail.com>
2024-10-26 20:27:44 +0500

    added email verification for new user

--- services/emailService.js
+++ services/emailService.js
@@ -5,22 +5,23 @@ const transporter = nodemailer.createTransport({
     host:'smtp.gmail.com',
     port:465,
     auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS
+        user:'haidermusil42@gmail.com',
+        pass:'xhkijjkfnyqmuncf'
     }
 })
 
-const sendPasswordResetEmail = (to,resetToken) => {
+const sendEmail = (to,subject,text) => {
     
-    const resetUrl = `http://localhost:5000/api/users/reset-password?token=${resetToken}`; // Update with your frontend URL
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to,
-        subject: 'Password Reset',
-        text: `You requested a password reset. Click the link to reset your password: ${resetUrl}`
+        subject,
+        text
     }
     
     return transporter.sendMail(mailOptions)
 }
 
-module.exports = { sendPasswordResetEmail }
+
+
+module.exports = { sendEmail }
